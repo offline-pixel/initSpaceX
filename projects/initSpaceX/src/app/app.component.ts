5e9ed37c3a32a2eabe2a1f17791e230e8267a2f7
deepak <d.ranolia92@gmail.com>
2020-09-09 22:45:57 +0530

    running

--- projects/initSpaceX/src/app/app.component.ts
+++ projects/initSpaceX/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'initSpaceX';
+export class AppComponent implements OnInit {
+  constructor( private swUpdate: SwUpdate ) { }
+  ngOnInit(): void {
+    if ( this.swUpdate.isEnabled ) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('App outdated. Update now?' )) {
+          window.location.reload();
+        }
+      });
+    }
+  }
+
 }
